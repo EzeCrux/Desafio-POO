14f2841cafd72288dbe9329520bfc347cbfbc718
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 15:31:09 -0300

    Add files via upload
    Codigo da Questão 20

---
+++ questao20/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Jogo
+{
+    public string Nome { get; set; }
+    public string Plataforma { get; set; }
+
+    public Jogo(string nome, string plataforma)
+    {
+        Nome = nome;
+        Plataforma = plataforma;
+    }
+
+    public void Jogar()
+    {
+        Console.WriteLine($"Você está jogando {Nome} no {Plataforma}! ECA");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Jogo jogo = new Jogo("League of Legends", "PC");
+        jogo.Jogar();
+    }
+}
