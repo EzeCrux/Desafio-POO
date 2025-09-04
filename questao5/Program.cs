1608c988023bc8b49de73912f676a20e5ea6847f
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 09:27:05 -0300

    Add files via upload

---
+++ questao5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Quadrado
+{
+    public double Lado;
+
+    public Quadrado(double lado)
+    {
+        Lado = lado;
+    }
+
+    public double Area()
+    {
+        return Lado * Lado;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Quadrado meuQuadrado = new Quadrado(5);
+
+        Console.WriteLine("Lado do quadrado: " + meuQuadrado.Lado);
+        Console.WriteLine("Área do quadrado: " + meuQuadrado.Area());
+    }
+}
