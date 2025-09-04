ddca5d9f293e441abcd7eec70dc01abe4a40e4fb
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 15:59:02 -0300

    Add files via upload
    Codigo da Questão 30

---
+++ questao30/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Configuracao
+{
+    private Dictionary<string, string> configuracoes;
+
+    public Configuracao()
+    {
+        configuracoes = new Dictionary<string, string>();
+    }
+
+    public void SetConfiguracao(string chave, string valor)
+    {
+        configuracoes[chave] = valor;
+    }
+
+    public string GetConfiguracao(string chave)
+    {
+        if (configuracoes.ContainsKey(chave))
+            return configuracoes[chave];
+        else
+            return "Chave não encontrada";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Configuracao config = new Configuracao();
+        config.SetConfiguracao("tema", "escuro");
+        config.SetConfiguracao("volume", "80");
+
+        Console.WriteLine(config.GetConfiguracao("tema"));
+        Console.WriteLine(config.GetConfiguracao("volume"));
+        Console.WriteLine(config.GetConfiguracao("brilho"));
+    }
+}
