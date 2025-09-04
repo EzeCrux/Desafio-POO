8c655a49fd641754158941446123a37d86649161
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 15:42:01 -0300

    Add files via upload
    Codigo da Questão 23

---
+++ questao23/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Aluno
+{
+    private double nota;
+
+    public double Nota
+    {
+        get { return nota; }
+        set
+        {
+            if (value >= 0 && value <= 10)
+                nota = value;
+            else
+                Console.WriteLine("Nota inválida. Insira um valor entre 0 e 10.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Aluno aluno = new Aluno();
+        aluno.Nota = 12;
+        Console.WriteLine($"Nota: {aluno.Nota}");
+        aluno.Nota = 9;
+        Console.WriteLine($"Nota: {aluno.Nota}");
+    }
+}
