db4042e0d03cd1ea4274004fdf0648012d08c067
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 15:48:41 -0300

    Add files via upload
    Codigo da Questão 26

---
+++ questao26/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Usuario
+{
+    private string senha;
+
+    public Usuario(string senhaInicial)
+    {
+        senha = senhaInicial;
+    }
+
+    public bool Autenticar(string senhaInformada)
+    {
+        return senhaInformada == senha;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Usuario usuario = new Usuario("1234");
+
+        Console.WriteLine(usuario.Autenticar("1234")); // true
+        Console.WriteLine(usuario.Autenticar("0000")); // false
+    }
+}
