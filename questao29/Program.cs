e4ee15ab46a8412eb33c5aca6914bba02994332f
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 15:54:43 -0300

    Add files via upload
    Codigo da Questão 29

---
+++ questao29/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Empregado
+{
+    private double salario;
+
+    public Empregado(double salarioInicial)
+    {
+        salario = salarioInicial;
+    }
+
+    public double CalcularBonus()
+    {
+        return salario * 0.1;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Empregado empregado = new Empregado(2000);
+        Console.WriteLine($"Bônus: {empregado.CalcularBonus()}");
+    }
+}
