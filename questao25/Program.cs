810a7b7227bf7a09a96dbb4c2364a0169a1a1414
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 15:47:25 -0300

    Add files via upload
    Codigo da Questão 25

---
+++ questao25/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Carro
+{
+    private double velocidade;
+
+    public Carro()
+    {
+        velocidade = 0;
+    }
+
+    public void Acelerar(double incremento)
+    {
+        if (incremento < 0)
+        {
+            Console.WriteLine("Incremento inválido.");
+            return;
+        }
+
+        velocidade += incremento;
+        if (velocidade > 200)
+            velocidade = 200;
+
+        Console.WriteLine($"Velocidade atual: {velocidade} km/h");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Carro carro = new Carro();
+        carro.Acelerar(50);
+        carro.Acelerar(180);
+        carro.Acelerar(-20);
+    }
+}
