d318ce9b9263f14d41968f06b43e4dba5e6a149b
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 09:34:27 -0300

    Add files via upload
    Codigo da Questão 7

---
+++ questao7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Circulo
+{
+    public double Raio;
+    public Circulo(double raio)
+    {
+        Raio = raio;
+    }
+
+    public double Area()
+    {
+        return Math.PI * Raio * Raio;
+    }
+
+    public double Perimetro()
+    {
+        return 2 * Math.PI * Raio;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Circulo meuCirculo = new Circulo(4);
+
+        Console.WriteLine("Raio do círculo: " + meuCirculo.Raio);
+        Console.WriteLine("Área do círculo: " + meuCirculo.Area());
+        Console.WriteLine("Perímetro do círculo: " + meuCirculo.Perimetro());
+    }
+}
