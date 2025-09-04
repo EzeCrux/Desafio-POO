6f101acb3dd2615f7cbbcf4132040de58de863d0
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 15:27:59 -0300

    Add files via upload
    Codigo da Questão 18

---
+++ questao18/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Calculadora
+{
+    public Calculadora() { }
+
+    public double Somar(double a, double b)
+    {
+        return a + b;
+    }
+
+    public double Subtrair(double a, double b)
+    {
+        return a - b;
+    }
+
+    public double Multiplicar(double a, double b)
+    {
+        return a * b;
+    }
+
+    public double Dividir(double a, double b)
+    {
+        return a / b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Calculadora calc = new Calculadora();
+
+        Console.WriteLine(calc.Somar(10, 5));
+        Console.WriteLine(calc.Subtrair(10, 5));
+        Console.WriteLine(calc.Multiplicar(10, 5));
+        Console.WriteLine(calc.Dividir(10, 5));
+    }
+}
