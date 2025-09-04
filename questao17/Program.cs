387be635bed193a3acb1b201eec78b870658f740
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 15:25:49 -0300

    Add files via upload
    Codigo da questão 17

---
+++ questao17/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Biblioteca
+{
+    private List<string> livros;
+
+    public Biblioteca()
+    {
+        livros = new List<string>();
+    }
+
+    public void AdicionarLivro(string titulo)
+    {
+        livros.Add(titulo);
+    }
+
+    public void MostrarLivros()
+    {
+        foreach (var livro in livros)
+        {
+            Console.WriteLine(livro);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.AdicionarLivro("O Senhor dos Anéis");
+        biblioteca.AdicionarLivro("Harry Potter");
+        biblioteca.AdicionarLivro("Dom Casmurro");
+
+        biblioteca.MostrarLivros();
+    }
+}
