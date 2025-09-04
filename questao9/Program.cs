2832399856e18f4333887ca18b5806acb2b4af10
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 09:48:27 -0300

    Add files via upload
    Codigo da Questão 9

---
+++ questao9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Produto
+{
+    public string Nome;
+    public double Preco;
+
+    public void AplicarDesconto()
+    {
+        Preco = Preco * 0.9; // reduz 10%
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Produto produto1 = new Produto();
+        produto1.Nome = "Camisa";
+        produto1.Preco = 100.0;
+
+        Console.WriteLine($"Produto: {produto1.Nome}");
+        Console.WriteLine($"Preço antes do desconto: R${produto1.Preco}");
+
+        produto1.AplicarDesconto();
+
+        Console.WriteLine($"Preço depois do desconto: R${produto1.Preco}");
+    }
+}
