0763a64f76756b4a1137494ac96998f78938c22e
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 15:23:45 -0300

    Add files via upload
    Codigo da Questão 16

---
+++ questao16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Usuario
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+
+    public Usuario(string nome, string email)
+    {
+        Nome = nome;
+        Email = email;
+    }
+
+    public void AlterarEmail(string novoEmail)
+    {
+        Email = novoEmail;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Usuario usuario = new Usuario("Ezequiel", "ezequiel@email.com");
+        Console.WriteLine($"Nome: {usuario.Nome}, Email: {usuario.Email}");
+
+        usuario.AlterarEmail("novoemail@email.com");
+        Console.WriteLine($"Nome: {usuario.Nome}, Email atualizado: {usuario.Email}");
+    }
+}
