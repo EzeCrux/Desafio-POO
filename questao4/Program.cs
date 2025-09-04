dfce94626cbd803ce60ea7a6d3c89c0463746251
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 09:21:55 -0300

    Add files via upload
    Codigo da questão 4

---
+++ questao4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Livro
+{
+   
+    public string Titulo;
+    public string Autor;
+
+    public Livro(string titulo, string autor)
+    {
+        Titulo = titulo;
+        Autor = autor;
+    }
+
+    public void ExibirInfo()
+    {
+        Console.WriteLine("Título: " + Titulo);
+        Console.WriteLine("Autor: " + Autor);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Livro meuLivro = new Livro("Pequeno Principe", "Antoine de Saint-Exupéry\r\n");
+
+        meuLivro.ExibirInfo();
+    }
+}
