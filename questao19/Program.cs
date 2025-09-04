d90c2675cd986483328e8a89a1824db887062230
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 15:29:22 -0300

    Add files via upload
    Codigo da Questão 19

---
+++ questao19/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Agenda
+{
+    private List<string> contatos;
+
+    public Agenda(List<string> contatosIniciais)
+    {
+        contatos = contatosIniciais;
+    }
+
+    public string BuscarContato(string nome)
+    {
+        foreach (var contato in contatos)
+        {
+            if (contato.ToLower() == nome.ToLower())
+                return contato;
+        }
+        return "Contato não encontrado";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<string> listaContatos = new List<string> { "Ana", "Bruno", "Carlos" };
+        Agenda agenda = new Agenda(listaContatos);
+
+        Console.WriteLine(agenda.BuscarContato("Bruno"));
+        Console.WriteLine(agenda.BuscarContato("Mariana"));
+    }
+}
