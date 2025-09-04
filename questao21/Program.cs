73959fcd053851f96bc0bb157e21f0adb6e53af4
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 15:32:34 -0300

    Add files via upload
    Codigo da Questão 21

---
+++ questao21/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Pessoa
+{
+    private int _idade;
+
+    public int Idade
+    {
+        get { return _idade; }
+        set { _idade = value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Pessoa pessoa = new Pessoa();
+        pessoa.Idade = 17;
+        Console.WriteLine($"Idade: {pessoa.Idade}");
+    }
+}
