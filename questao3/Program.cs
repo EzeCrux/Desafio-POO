8a64cc851b5a9c871efa89f93e9cbebb78bdcc3d
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 09:05:05 -0300

    Add files via upload
    Codigo da Atv3

---
+++ questao3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Animal
+{
+    public string Nome;
+    public int Idade;
+
+    public Animal(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal meuAnimal = new Animal("Salsichinha", 2);
+
+        Console.WriteLine("Nome do animal: " + meuAnimal.Nome);
+        Console.WriteLine("Idade do animal: " + meuAnimal.Idade + " anos");
+    }
+}
