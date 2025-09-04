d8f481f3bcddc84c09ec6bcbd970048e21605bf9
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 10:39:44 -0300

    Add files via upload
    Codigo da Questão 14

---
+++ questao14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Veiculo
+{
+    public string Marca;
+    public string Modelo;
+
+    public Veiculo(string marca, string modelo)
+    {
+        Marca = marca;
+        Modelo = modelo;
+    }
+
+    public void Acelerar()
+    {
+        Console.WriteLine("Acelerando!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Veiculo v1 = new Veiculo("Toyota", "Corolla");
+        Console.WriteLine($"Marca: {v1.Marca}, Modelo: {v1.Modelo}");
+        v1.Acelerar();
+    }
+}
