97e5c5d96e135239012fef2f69d5d3304ddb9123
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 10:29:03 -0300

    Add files via upload
    Codigo da Questão 12

---
+++ questao12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Funcionario
+{
+    public string Nome;
+    public double Salario;
+
+    public Funcionario(string nome, double salario)
+    {
+        Nome = nome;
+        Salario = salario;
+    }
+
+    public void AumentarSalario(double percentual)
+    {
+        Salario += Salario * (percentual / 100);
+    }
+
+    public void MostrarDados()
+    {
+        Console.WriteLine($"Nome: {Nome}, Salário: {Salario:F2}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Funcionario f1 = new Funcionario("Ezequiel", 2000);
+        f1.MostrarDados();
+
+        f1.AumentarSalario(10);
+        f1.MostrarDados();
+    }
+}
