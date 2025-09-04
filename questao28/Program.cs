f5f13eeb688eda64f7d8f0d322a3fd31b74b3c17
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 15:53:24 -0300

    Add files via upload
    Codigo da Questão 28

---
+++ questao28/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Forma
+{
+    private double area;
+
+    public Forma(double largura, double altura)
+    {
+        area = largura * altura;
+    }
+
+    public double Area
+    {
+        get { return area; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Forma forma = new Forma(5, 10);
+        Console.WriteLine($"Área: {forma.Area}");
+    }
+}
