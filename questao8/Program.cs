4d3698a3df50828537e574eac2b0863f8c821d16
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-09-04 09:39:25 -0300

    Add files via upload
    Codigo da Questão 8

---
+++ questao8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class ContaBancaria
+{
+    public double Saldo = 382;
+
+    public void Depositar(double valor)
+    {
+        Saldo += valor;
+        Console.WriteLine("Depósito de R$" + valor + " realizado com sucesso!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaBancaria conta = new ContaBancaria();
+
+        Console.WriteLine("Saldo inicial: R$" + conta.Saldo);
+
+        conta.Depositar(1932.50);
+
+        Console.WriteLine("Saldo atual: R$" + conta.Saldo);
+    }
+}
